94685e119989dbdb9368315bd015f9aaaa021c99
toothrot <scissorjammer@gmail.com>
2008-10-21 12:38:46 -0500

    The timeline can now be refreshed

--- gutter.rb
+++ gutter.rb
@@ -16,27 +16,37 @@ class Gutter
 
 end
 
-Shoes.app do
-  gtter = Gutter.new
-  twit = Twitter::Base.new(gtter.user, gtter.password)
-  stack :height => height - 25, :scroll => true do
-    twit.timeline(:friends).each do |status|
-      tweet = flow :margin => [5,5,5,5] do
-        background white
-        border(black, :strokewidth => 2)
-        stack :width => 50, :margin => [2,2,2,2] do
-          image status.user.profile_image_url
+module GutterUI
+  def draw_timeline
+    @timeline = stack :height => height - 35, :scroll => true do
+      @twit.timeline(:friends).each do |status|
+        tweet = flow :margin => [5,5,5,5] do
+          background white
+          border(black, :strokewidth => 2)
+          stack :width => 50, :margin => [2,2,2,2] do
+            image status.user.profile_image_url
+          end
+          stack :width => 500 - width do
+            inscription(strong("#{status.user.name}: "), status.text, :margin_left => 20)
+          end
         end
-        stack :width => 500 - width do
-          inscription(strong("#{status.user.name}: "), status.text, :margin_left => 20)
-        end
-      end
-    end # end timeline
+      end # end timeline
+    end
   end
+end
+
+Shoes.app do
+  extend GutterUI 
+  gtter = Gutter.new
+  @twit = Twitter::Base.new(gtter.user, gtter.password)
+  draw_timeline
   flow do 
-    tweet_text = edit_line "tweet", :width => width - 150
+    tweet_text = edit_line "tweet", :width => width - 250
     button "blag" do
-      twit.post(tweet_text.text)
+      @twit.post(tweet_text.text)
+    end
+    button "refresh" do
+      @timeline.clear { draw_timeline }
     end
   end
 end
