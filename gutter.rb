26c28fd1e026cf97d959adf65ef8bb8867c53800
Alexander Rakoczy <scissorjammer@gmail.com>
2008-10-21 19:58:59 -0500

    better reply linking

--- gutter.rb
+++ gutter.rb
@@ -17,24 +17,24 @@ class Gutter
 end
 
 module GutterUI
+  def reply(status)
+    @tweet_text.text = "@#{status.user.screen_name}: "
+  end
+
   def draw_timeline
-    @timeline = flow :height => height - 35, :scroll => true do
+    @timeline = stack :height => height - 35, :width => width, :scroll => true do
       @twit.timeline(:friends).each do |status|
         tweet = flow :margin => [5,5,20,5] do
           background '#202020', :curve => 8
           border dimgray, :curve => 8
           stack :width => 50, :margin => [4,4,2,4] do
             image status.user.profile_image_url
+            click { reply(status) }
           end
-          stack :width => 500 - width do
-            inscription(strong("#{status.user.name}: ", :stroke => darkorange), status.text, :margin_left => 20, :stroke => white)
-          end
-          click do
-            @tweet_text.text = "@#{status.user.screen_name}: "
+          flow :width => 500 - width do
+            inscription(strong("#{status.user.name}: ", :stroke => darkorange), status.text, ' ', link('reply', :click => lambda {reply(status)}), :margin_left => 20, :stroke => white)
           end
         end # end tweet
-        tweet.hover { |twe| twe.border yellow, :curve => 8 }
-        tweet.leave { |twe| twe.border dimgray, :curve => 8 }
       end # end twit
     end # end timeline
   end
@@ -43,13 +43,14 @@ end
 Shoes.app do
   extend GutterUI 
   background black
+  stroke white
   gtter = Gutter.new
   @twit = Twitter::Base.new(gtter.user, gtter.password)
   @timeline = flow :height => height - 35, :scroll => true do
     para "loading"
   end
   flow do 
-    @tweet_text = edit_line "tweet", :width => width - 250
+    @tweet_text = edit_line "", :width => width - 250
     button "blag" do
       @twit.post(tweet_text.text)
     end
@@ -58,4 +59,7 @@ Shoes.app do
     end
   end
   @timeline.clear { draw_timeline }
+  timer(60*6) do
+    @timeline.clear { draw_timeline }
+  end
 end
