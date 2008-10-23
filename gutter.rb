567820ac0db1889988aa07edfa4f010b0958fef7
Alexander Rakoczy <scissorjammer@gmail.com>
2008-10-22 23:28:51 -0500

    a nice bit cleaner thanks to the shoes mailing list

--- gutter.rb
+++ gutter.rb
@@ -9,7 +9,8 @@ class Gutter
   attr_accessor :password
 
   def initialize
-    conf = YAML::load(File.open(File.join("#{ENV['HOME'] || ENV['USERPROFILE']}",'.gutter.yml')))
+    @filename = File.join("#{ENV['HOME'] || ENV['USERPROFILE']}",'.gutter.yml')
+    conf = YAML::load_file(@filename)
     @user = conf["gutter"]["login"]
     @password = conf["gutter"]["password"]
   rescue
@@ -17,7 +18,8 @@ class Gutter
   end
 
   def save
-    YAML::dump({'gutter' => {'login' => user, 'password' => password}}, File.open(File.join("#{ENV['HOME'] || ENV['USERPROFILE']}",'.gutter.yml'), 'w'))
+    YAML::dump({'gutter' => {'login' => user, 'password' => password}}, 
+      File.open(@filename, 'w'))
   rescue
     puts "Can't open preferences file"
   end
