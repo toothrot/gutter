ad449317c03464ecce6a58fb55c0cb32de00f444
toothrot <scissorjammer@gmail.com>
2008-11-25 13:38:44 -0600

    Fixed the input box to the top of the window.

--- gutter.rb
+++ gutter.rb
@@ -18,10 +18,12 @@ class Twitter::Status
   end
 end
 
-Shoes.app :title => 'Gutter', :width => 450 do
+Shoes.app :title => 'Gutter', :width => 450, :scroll => false do
   extend GutterUI 
   extend Notify
   extend TinyURLSupport
+
+  app.slot.scroll(false)
 
   background black
   stroke white
@@ -44,40 +46,47 @@ Shoes.app :title => 'Gutter', :width => 450 do
   end
   ## - end setup
 
-  stack do
-    flow do # - header
-      background '#202020'
-      border dimgray
-      flow :margin => [5,5,5,0] do
-        @tweet_text = edit_line("", :width => width - 140) do |e| 
-          @counter.text =  140 - (e.text.size || 0)
-        end
-        keypress { |k| send_tweet.call if (k == :enter) || (k == "\n") }
-        @blag = stack :width => 40, :margin_left => 4, :margin_right => 4 do
-          background '#303030'
-          border dimgray
-          inscription "blag", :margin => [4]*4, :stroke => white
-          hover { @blag.border gray }
-          leave { @blag.border dimgray }
-          click { send_tweet.call }
-          release { @blag.border gray }
-        end
-        image('http://toothrot.nfshost.com/gutter/icons/arrow_refresh.png', :click => lambda { @timeline.clear { draw_timeline } }, :margin => [5,5,5,5] )
-        para "| ", :stroke => gray
-        @counter = strong("140")
-        para @counter, :stroke => white
-      end
-    end # - header
-
-    @timeline = stack :margin => [0,5,0,0] do
-      para "loading"
-    end
-
+  @timeline = stack :margin => [0,42,0,0] do
+    para "loading"
   end
+
+  flow :attach => Window, :top => 0, :left => 0, :height => 40, :width => width - gutter do # - header
+    background '#202020'
+    border dimgray
+    flow :margin => [5,5,5,0] do
+      @tweet_text = edit_line("", :width => width - 140) do |e| 
+        @counter.text =  140 - (e.text.size || 0)
+      end
+      @blag = stack :width => 40, :margin_left => 4, :margin_right => 4 do
+        background '#303030'
+        border dimgray
+        inscription "blag", :margin => [4]*4, :stroke => white
+        hover { @blag.border gray }
+        leave { @blag.border dimgray }
+        click { send_tweet.call }
+        release { @blag.border gray }
+      end
+      image('http://toothrot.nfshost.com/gutter/icons/arrow_refresh.png', :click => lambda { @timeline.clear { draw_timeline } }, :margin => [5,5,5,5] )
+      para "| ", :stroke => gray
+      @counter = strong("140")
+      para @counter, :stroke => white
+    end
+  end # - header
+
+  keypress do |k|
+    send_tweet.call if (k == :enter) || (k == "\n")
+    @timeline.scroll_top += 3 if k == :up
+    @timeline.scroll_top -= 3 if k == :down
+  end
+
 
   @timeline.clear { draw_timeline }
   every(60*6) do
     @timeline.clear { draw_timeline }
+  end
+
+  every(1) do
+    @timeline.style(:height => height - 45)
   end
 
 end
