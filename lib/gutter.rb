447f76e6dc0663ea0842ef913035180e05c69102
Kiss Péter <ypetya@gmail.com>
2009-01-24 21:48:55 +0100

    Config fix.
     * Config file parse moved to Class method.
     * New config attribute 'width'.
     * Fixed: load login and password field at startup
    
    
    Signed-off-by: Kiss Péter <ypetya@gmail.com>

--- lib/gutter.rb
+++ lib/gutter.rb
@@ -2,18 +2,40 @@ class Gutter
   attr_accessor :user, :password
 
   def initialize
-    @filename = File.join("#{ENV['HOME'] || ENV['USERPROFILE']}",'.gutter.yml')
-    conf = YAML::load_file(@filename)
-    @user = conf["gutter"]["login"]
-    @password = conf["gutter"]["password"]
-  rescue
-    @user, @password = nil, nil
+    @user = Gutter.login
+    @password = Gutter.password
+  end
+
+  def destroy
+    save
+  end
+
+  def self.method_missing m,*args
+    Gutter.load_conf
+    return @conf["gutter"][m.to_s] if @conf["gutter"]
   end
 
   def save
-    YAML::dump({'gutter' => {'login' => user, 'password' => password}}, 
-      File.open(@filename, 'w'))
+    Gutter.save( @user, @password)
+  end
+
+  def self.load_conf
+    @conf = nil unless defined?(@conf)
+    Gutter.get_conf unless @conf
+  end
+
+  def self.save user,password
+    Gutter.load_conf
+    @conf["gutter"]["login"] = user
+    @conf["gutter"]["password"] = password
+    YAML::dump(@conf.to_yaml, File.open(@filename, 'w'))
   rescue
     puts "Can't open preferences file"
   end
+
+  def self.get_conf
+    @filename = File.join("#{ENV['HOME'] || ENV['USERPROFILE']}",'.gutter.yml')
+    @conf = YAML::load_file(@filename)
+  end
+
 end
